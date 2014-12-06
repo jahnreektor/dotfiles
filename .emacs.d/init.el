4aed071d7165be19afe362da3f7cd57e948007a4
John Richter <johndavidrichter@gmail.com>
2014-12-06 05:55:49 +0000

    Typescript changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -205,3 +205,22 @@
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
+
+;; Typescript
+
+;; If use bundled typescript.el,
+(require 'typescript)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+(require 'tss)
+
+;; Key binding
+(setq tss-popup-help-key "C-:")
+(setq tss-jump-to-definition-key "C->")
+(setq tss-implement-definition-key "C-c i")
+
+;; Make config suit for you. About the config item, eval the following sexp.
+;; (customize-group "tss")
+
+;; Do setting recommemded configuration
+(tss-config-default)
--- .emacs.d/mustache-mode.el
+++
@@ -1,1 +0,0 @@
-<html><body>You are being <a href="https://raw.githubusercontent.com/mustache/emacs/master/mustache-mode.el">redirected</a>.</body></html>
