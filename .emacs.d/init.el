f89be20a3c4534adb97e3e61d0bea3df531f9fbe
John Richter <johndavidrichter@gmail.com>
2014-12-06 06:36:13 +0000

    Added yasnippet and angular snippets

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -206,6 +206,23 @@
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
 
+;;yasnippet from truongtx.me
+;;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;; auto complete mod
+;;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+
 ;; Typescript
 
 ;; If use bundled typescript.el,
@@ -224,3 +241,9 @@
 
 ;; Do setting recommemded configuration
 (tss-config-default)
+
+
+;;js2-mode
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+(setq js2-highlight-level 3)
